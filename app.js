899f5010be83c56aaf42ec4d227453dbc6f9f491
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-05-09 13:00:58 -0400

    Added Dark Sky API

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// Test
+const request = require('request');
+
+// url: At end, you have API key followed by latitude & longitude followed by optional query strings
+const url = 'https://api.darksky.net/forecast/KEY/37.8267,-122.4233?units=si';
+
+// 1st param = url; 2nd = fn to run. Response = data received from HTTP request (JSON format).
+// The weather info is stored in body of request.
+// Use json method to have request automatically parse JSON body data
+request({ url: url, json: true }, (error, response) => {
+	let temp = response.body.currently.temperature;
+	let rain = response.body.currently.precipProbability;
+	let todaySummary = response.body.daily.data[0].summary;
+	console.log(todaySummary + ' It is currently ' + temp + ' degrees out. There is a ' + rain + '% chance of rain');
+})

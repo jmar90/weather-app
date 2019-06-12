178a176bae4c1418d1f72884562653fabbdc0aa1
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-06-12 13:40:47 -0400

    Pulled geocode & forecast callbacks into separate files

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
-const request = require('request');
-const DARKSKY_KEY = process.env.DARKSKY_KEY;
-const MAPBOX_KEY = process.env.MAPBOX_KEY;
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-// url: At end, you have API key followed by latitude & longitude followed by optional query strings
-const url = 'https://api.darksky.net/forecast/' + DARKSKY_KEY + '/37.826,-122.4233?units=si';
-
-// 1st param = url; 2nd = fn to run. Response = data received from HTTP request (JSON format).
-// The weather info is stored in body of request.
-// Use json method to have request automatically parse JSON body data
-request({ url: url, json: true }, (error, response) => {
-	if(error){ //If low-level OS error (eg, no internet or API down)
-		console.log('Unable to connect to weather service.');
-	} else if(response.body.error){
-		console.log('Unable to find lcoation');
-	} else {
-		let temp = response.body.currently.temperature;
-		let rain = response.body.currently.precipProbability;
-		let todaySummary = response.body.daily.data[0].summary;
-		console.log(todaySummary + ' It is currently ' + temp + ' degrees out. There is a ' + rain + '% chance of rain');
-	}
+// Call geocode function
+geocode('Boston', (error, data) => {
+	console.log('Error: ', error);
+	console.log('Data: ', data);
 })
 
-// Geocoding API: user will enter address, API converts to lat/long, & weather forecast gets displayed
-const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=' + MAPBOX_KEY;
-
-request({ url: geoCodeURL, json: true }, (error, response) => {
-	if(error){
-		console.log('Unable to connect to location services.');
-	} else if(response.body.features.length === 0){ //If there is an error, the length of features array will be 0
-		console.log('Unable to find location. Try again with a different search term.');
-	} else {
-		let longitude = response.body.features[0].center[0];
-		let latitude = response.body.features[0].center[1];
-		console.log(longitude + ', ' +latitude);
-	}
+// Call weather function
+forecast(-71.0596, 42.3605, (error, data) => {
+	console.log('Error: ', error);
+	console.log('Data: ', data);
 })
